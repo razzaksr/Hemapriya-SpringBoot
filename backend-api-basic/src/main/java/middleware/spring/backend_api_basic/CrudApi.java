6ba54ffeffe94e371d0f3c61a59dec5878bf2944
razzaksr <razzaksr@gmail.com>
2024-11-04 17:41:50 +0530

    basic crud

--- backend-api-basic/src/main/java/middleware/spring/backend_api_basic/CrudApi.java
+++ backend-api-basic/src/main/java/middleware/spring/backend_api_basic/CrudApi.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CrudApi {
     // Storage: collections, temporary
     private List<String> mySkills=Stream.of("Java","Python","DJango","MongoDB").collect(Collectors.toList());
+
+    @PutMapping("/{index}")
+    public String update(@PathVariable("index") int index,@RequestBody String replacable){
+        if(index>0&&index<mySkills.size()){
+            
+            return replacable+" has been updated @ "+index;
+        }
+        return replacable+" hasn't been updated @ "+index;
+    }
 
     @DeleteMapping("/{position}")
     public String erase(@PathVariable("position") int position){
