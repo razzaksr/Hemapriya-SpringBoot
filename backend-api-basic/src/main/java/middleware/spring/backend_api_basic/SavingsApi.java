d4b2c4b4de008b2205349a4389a3f75015c40ac7
razzaksr <razzaksr@gmail.com>
2024-11-05 18:38:37 +0530

    Pojo based read, update

--- backend-api-basic/src/main/java/middleware/spring/backend_api_basic/SavingsApi.java
+++ backend-api-basic/src/main/java/middleware/spring/backend_api_basic/SavingsApi.java
@@ -5,7 +5,9 @@ import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,18 +15,56 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/savings")
 public class SavingsApi {
+    // storage: temporary
     List<Savings> accounts = Stream.of(
         new Savings("Razak Mohamed S",8765678987L,259.6,"ABCD00001","ABC Bank"),
         new Savings("Zealous Tech Corp",765678987644L,12959.6,"ABCD00001","ABC Bank"),
-        new Savings("Sabarinathan S",9876567894L,9446.6,"ABCD01041","BCD Bank")
+        new Savings("Sabarinathan S",9876567894L,9446.6,"ABCD01041","BCD Bank"),
+        new Savings("Rasheedha R",1121323233434L,1446.6,"BCD010241","BCD Bank"),
+        new Savings("Rajiya R",949349345443434L,13446.6,"BCD010241","ABC Bank")
     ).collect(Collectors.toList());
 
+    @PutMapping("/")
+    public String updateAccount(@RequestBody Savings acc){
+        for(int index=0;index<accounts.size();index++){
+            // find the index of matched account number from storage
+            if(accounts.get(index).getAccountNumber()==acc.getAccountNumber()){
+                // update the matched account details
+                accounts.set(index, acc);
+                return acc.getAccountNumber()+" has updated";
+            }
+        }
+        return acc.getAccountNumber()+" hasn't updated";
+    }
+
+    // read by account number
+    @GetMapping("/accnum/{number}")
+    public Savings readByAccountNumber(@PathVariable("number") long number){
+        try{
+            return accounts.stream().filter(obj-> obj.getAccountNumber()==number).toList().get(0);
+        }
+        catch(ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            return null;
+        }
+    }
+
+    // read one pojo by position
+    @GetMapping("/{index}")
+    public Savings readByIndex(@PathVariable("index") int index){
+        if(index>=0&&index<accounts.size()){
+            return accounts.get(index);
+        }
+        return null;
+    }
+
+    // create
     @PostMapping("/")
     public String create(@RequestBody Savings savings){
         accounts.add(savings);
         return savings.getAccountHolder()+" has opened savings account";
     }
 
+    // read existing 
     @GetMapping("/")
     public List<Savings> readAll(){
         return accounts;
