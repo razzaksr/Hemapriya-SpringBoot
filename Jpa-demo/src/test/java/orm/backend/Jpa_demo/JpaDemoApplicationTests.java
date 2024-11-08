c0d17a119a39704b2294d955ff16dd0ba3449ea5
razzaksr <razzaksr@gmail.com>
2024-11-08 18:37:45 +0530

    Hibernate table created

---
+++ Jpa-demo/src/test/java/orm/backend/Jpa_demo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package orm.backend.Jpa_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
