d191788f75430e7ded0a06449b5c5b19e23f941d
razzaksr <razzaksr@gmail.com>
2024-12-25 16:51:59 +0530

    load balace done

--- Microservices/customer-service/src/main/java/microservice/one/customer_service/FeignToAccount.java
+++ Microservices/customer-service/src/main/java/microservice/one/customer_service/FeignToAccount.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="feign",url = "http://localhost:8092")
+// @FeignClient(name="feign",url = "http://localhost:8092")
+@FeignClient(name = "ACCOUNT-SERVICE")
 public interface FeignToAccount {
     @GetMapping("/account/{customerId}")
     List<Account> receiveAccount(@PathVariable("customerId") int customerId);
